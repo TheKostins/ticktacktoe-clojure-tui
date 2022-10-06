419e79cf46c0e4879cf462baf65c991f634398df
Konstantin Anisimov <anisimovkosta19@gmail.com>
2022-10-06 15:21:56 +0300

    Initial commit

---
+++ test/clj_ticktacktoe_tui/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-ticktacktoe-tui.core-test
+  (:require [clojure.test :refer :all]
+            [clj-ticktacktoe-tui.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
